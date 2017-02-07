420dab65ddf69ce380290db474e2315a1286ee25
Bryant Worcester <bryantw@WIN2.CS.KSU.EDU>
2017-02-07 14:23:37 -0600

    uploadImage added, errors included

--- server.js
+++ server.js
@@ -6,8 +6,12 @@
  * simple photo gallery web app.
  */
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 var port = 3027;
+
+var config = JSON.parse(fs.readFileSync('config.json'));
+
 
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
 var stylesheet = fs.readFileSync('gallery.css');
@@ -32,12 +36,20 @@ function imageNamesToTags(fileNames) {
 function buildGallery(imageTags) {
 	var html = '<!doctype html>';
 			html += '<head>';
-			html += 	'<title>Gallery</title>';
+			html += 	'<title>' + config.title + '</title>';
 			html += 	'<link href="gallery.css" rel="stylesheet" type="text/css"';
 			html += '</head>';
 			html += '<body>';
 			html += 	'<h1>Gallery</h1>';
+			html += 	'<form>';
+			html += 		'<input type="text" name="title">';
+			html += 		'<input type="submit" value="Change Gallery Title">';
+			html += 	'</form>';
 			html += imageNamesToTags(imageTags).join('');
+			html += '<form action="" method="POST" enctype="multipart/form-data">';
+			html += 	'<input type="file" name="image">';
+			html +=		'<input type="submit" value="Upload Image">';
+			html += '</form>';
 			html += '</body>';
 	return html;
 }
@@ -46,6 +58,29 @@ var gHtml = imageNames.map(function(fileName) {
 	return '<img src="' + fileName + '"alt="a fishing ace at work">';
 }).join('');
 */
+
+function uploadImage(req, res) {
+	var body='';
+	req.on('error', function() {
+		res.statusCode = 500;
+		res.end();
+	});
+	req.on('data', function(data) {
+		body += data;
+	});
+	req.on('end', function() {
+		fs.writeFile('filename', data, function(err) {
+			if(err) {
+				console.error(err);
+				res.statusCode = 500;
+				res.end();
+				return;
+			}
+			serveGallery(req, res);
+		});
+	});
+}
+
 function serveGallery(req, res) {
 	getImageNames(function(err, imageNames) {
 		if (err){
@@ -78,10 +113,24 @@ function serveImage(filename, req, res) {
 
 var server = http.createServer(function(req, res){
 
-	switch (req.url) {
+	var urlParts url.parse(req.url)
+
+	if(urlParts.query) {
+		var matches = /title=(.+)($|&)/.exec(urlParts.query);
+		if(matches && matches[1]) {
+			config.title = decodeURIComponent(matches[1]);
+			fs.writeFile('config.json', JSON.stringify(config));
+		}
+	}
+
+	switch (urlParts.pathname) {
 		case '/':
 		case '/gallery':
-			serveGallery(req, res);
+			if(req.method == 'GET') {
+				serveGallery(req, res);
+			} else if(req.method == 'POST') {
+				uploadImage(req, res);
+			}
 			break;
 		case '/gallery.css':
 			res.setHeader('Content-Type', 'text/css');
